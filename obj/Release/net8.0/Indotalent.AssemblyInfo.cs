5163904674ee6d1cbe3fccbc685deb6b89d51c26
Tarik <Tarik@DESKTOP-TCBJHDA>
2024-11-21 21:11:40 +0100

    push 6

--- obj/Release/net8.0/Indotalent.AssemblyInfo.cs
+++ obj/Release/net8.0/Indotalent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce88d8fec6c0f8089a20440b80a95fcd5deddc54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe53f317d6e84753ff2019f84ce2101500043109")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Indotalent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
